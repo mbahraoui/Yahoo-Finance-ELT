0cee86f143ddd2d6615abcd78d5052e603fc29e8
mbahraoui <mohamedbahraoui20@gmail.com>
2023-11-19 01:50:25 +0100

    transfer data from s3 to redshift;

--- dags/yahoo_elt_pipeline.py
+++ dags/yahoo_elt_pipeline.py
@@ -6,6 +6,7 @@ import pandas as pd
 import boto3
 from dotenv import load_dotenv
 import os
+import psycopg2
 
 
 default_args = {
@@ -57,8 +58,9 @@ def extract_data():
     index_data_df.to_csv(index_file_name, index=False)
 
 def load_to_s3():
-    aws_access_key_id = os.environ.get("ACCESS_KEY")
-    aws_secret_access_key = os.environ.get("SECRET")
+    load_dotenv()
+    aws_access_key_id = os.os.getenv("AWS_ACCESS_KEY")
+    aws_secret_access_key = os.os.getenv("AWS_SECRET_KEY")
     s3_bucket_name = 'yahoo-finance-data'
     s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
 
@@ -75,6 +77,96 @@ def load_to_s3():
     end_date = datetime.today().date()
     s3.upload_file(f"/tmp/stock_data_{end_date}.csv", s3_bucket_name, f"stock_data/stock_data_{end_date}.csv")
     s3.upload_file(f"/tmp/index_data_{end_date}.csv", s3_bucket_name, f"index_data/index_data_{end_date}.csv")
+
+
+def transfer_data_to_redshift():
+    load_dotenv()
+
+    redshift_endpoint = os.getenv("REDSHIFT_ENDPOINT")
+    redshift_port = os.getenv("REDSHIFT_PORT")
+    redshift_database = os.getenv("REDSHIFT_DATABASE")
+    redshift_user = os.getenv("REDSHIFT_USER")
+    redshift_password = os.getenv("REDSHIFT_PASSWORD")
+    aws_access_key = os.getenv("AWS_ACCESS_KEY")
+    aws_secret_key = os.getenv("AWS_SECRET_KEY")
+
+    redshift_conn = psycopg2.connect(
+    dbname=redshift_database,
+    host=redshift_endpoint,
+    port=redshift_port,
+    user=redshift_user,
+    password=redshift_password
+    )
+    redshift_cursor = redshift_conn.cursor()
+
+    company_info_staging = """
+    CREATE TABLE IF NOT EXISTS company_info_staging (
+        Symbol VARCHAR(50),
+        Name VARCHAR(255),
+        Sector VARCHAR(255),
+        Industry VARCHAR(255)
+        );
+    """
+    redshift_cursor.execute(company_info_staging)
+
+
+    stock_data_staging = """
+        CREATE TABLE IF NOT EXISTS stock_data_staging (
+        Symbol VARCHAR(10),
+        stock_Date DATE,
+        "Open" DECIMAL(20, 10),
+        High DECIMAL(20, 10),
+        Low DECIMAL(20, 10),
+        Close DECIMAL(20, 10),
+        AdjClose DECIMAL(20, 10),
+        Volume BIGINT
+        );
+    """
+    redshift_cursor.execute(stock_data_staging)
+
+
+    indices_info_staging = """
+        CREATE TABLE IF NOT EXISTS indices_info_staging (
+            Symbol VARCHAR(50),
+            Name VARCHAR(255),
+            Description VARCHAR(255)
+        );
+    """
+    redshift_cursor.execute(indices_info_staging)
+
+    index_data_staging = """
+        CREATE TABLE IF NOT EXISTS index_data_staging (
+        Symbol VARCHAR(10),
+        index_Date DATE,
+        "Open" DECIMAL(20, 10),
+        High DECIMAL(20, 10),
+        Low DECIMAL(20, 10),
+        Close DECIMAL(20, 10),
+        AdjClose DECIMAL(20, 10),
+        Volume BIGINT
+        );
+    """
+    redshift_cursor.execute(index_data_staging)
+
+
+    staging_table_names = ['company_info_staging', 'stock_data_staging', 'indices_info_staging', 'index_data_staging']
+    s3_bucket_name = 'yahoo-finance-data'
+    s3_keys= ['company_info.csv', 'stock_data/stock_data_2023-11-07.csv', 'indices_info.csv', 'index_data/index_data_2023-11-07.csv']
+
+    for staging_table_name, s3_key in zip(staging_table_names, s3_keys):
+        load_staging_data_sql = f"""
+            COPY {staging_table_name}
+            FROM 's3://{s3_bucket_name}/{s3_key}'
+            CREDENTIALS 'aws_access_key_id={aws_access_key};aws_secret_access_key={aws_secret_key}'
+            CSV
+            IGNOREHEADER 1;
+        """
+        redshift_cursor.execute(load_staging_data_sql)
+
+
+    redshift_conn.commit()
+    redshift_conn.close()
+
 
 
 
@@ -98,4 +190,9 @@ with DAG(
         python_callable=load_to_s3
     )
 
-    extract_data_task >> load_to_s3_task
+    transfer_data_to_redshift_task = PythonOperator(
+        task_id="transfer_data_to_redshift",
+        python_callable=
+    )
+
+    extract_data_task >> load_to_s3_task >> transfer_data_to_redshift
